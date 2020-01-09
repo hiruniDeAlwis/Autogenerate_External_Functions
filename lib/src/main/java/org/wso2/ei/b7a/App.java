26ca9fef851783216a975f9efbdd3473eef18acb
hiruniDeAlwis <praharsha.dealwis@gmail.com>
2020-01-09 11:50:21 +0530

    Adding constants

--- lib/src/main/java/org/wso2/ei/b7a/App.java
+++ lib/src/main/java/org/wso2/ei/b7a/App.java
@@ -1,68 +1,85 @@
 package org.wso2.ei.b7a;
 import java.lang.reflect.*;
+import java.lang.Class.*;
 import java.io.IOException;
 import java.io.FileWriter;
 
 public class App {
     static String classFullName;
+    private static final String HEADER = "import ballerinax/java";
+    private static final String FUNCTION ="function";
+    private static final String SPACE = " ";
+    private static final String BRACKETS = "()";
+    private static final String LEFTBRACKET = "(";
+    private static final String RIGHTBRACKET = ")";
+    private static final String LEFTBRACE = "{";
+    private static final String RIGHTBRACE = "}";
+    private static final String COMMA = ",";
+    private static final String RETURN= "returns handle ";
+    private static final String JAVAMETHOD = " = @java:Method";
+    private static final String METHODNAME = "name : ";
+    private static final String CLASSNAME = "class : ";
+    private static final String QUOTE = "\"";
+    private static final String EXTERNAL_KEYWORD = "external";
+    private static final String SEMICOLON = ";";
+    private static final String NEW_LINE = "\n";
+
 
     public static void main(String...args){
-        classFullName=giveFullClass();
-        String classSimpleName=giveSimpleClass();
-        writeMethod(classSimpleName,classFullName);
-    }
-
-    public static String giveFullClass() {
-
         TestJava obj = new TestJava();
         Class cls = obj.getClass();
+        classFullName=giveFullClass(cls);
+        writeMethod(cls,classFullName);
+    }
+
+    public static String giveFullClass(Class cls) {
+
         classFullName=cls.getName();
         return classFullName;
     }
 
-    public static String giveSimpleClass() {
+    public static String giveSimpleClass(Class cls) {
 
-        int firstChar= classFullName.lastIndexOf('.') + 1;
-        String classSimpleName = classFullName.substring(firstChar);
-        return classSimpleName;
+        String simpleName = cls.getSimpleName();
+        return simpleName;
     }
 
-    public static String writeMethod(String classSimpleName,String classFullName){
+    public static String writeMethod(Class cls,String classFullName){
+
 
         try {
             FileWriter wObj = new FileWriter("../Bal_Project/src/module_bal/external_functions.bal");
-            wObj.write("import ballerinax/java; \n");
-            TestJava obj = new TestJava();
-            Class cls = obj.getClass();
+            wObj.write(HEADER+SEMICOLON+NEW_LINE);
             Method[] methods = cls.getDeclaredMethods();
             for (Method method:methods) {
-                wObj.write("function ");
-                wObj.write(method.getName()+" ");
+                wObj.write(FUNCTION+SPACE);
+                wObj.write(method.getName());
 
                 Parameter[] parameter = method.getParameters();
                 if (parameter == null) {
-                    wObj.write("()");
+                    wObj.write(BRACKETS);
                 }
                 else{
-                    wObj.write("(");
+                    wObj.write(LEFTBRACKET);
                     for(int i=0;i<parameter.length;i++){
                         wObj.write(parameter[i].getParameterizedType() + " " + parameter[i].getName());
                         if(i<parameter.length-1){
-                            wObj.write(" , ");
+                            wObj.write(COMMA);
                         }
                     }
-                    wObj.write(")");
+                    wObj.write(RIGHTBRACKET);
                 }
                 Class returnParam = method.getReturnType();
                 String returnType=returnParam.getName();
                 if( returnType!= "void"){
-                    wObj.write("returns handle");
+                    wObj.write(RETURN);
                 }
-                wObj.write(" = @java:Method { name: \" ");
-                wObj.write(method.getName());
-                wObj.write(" \", class : \" ");
+                wObj.write(JAVAMETHOD+LEFTBRACE+NEW_LINE+METHODNAME+QUOTE);
+                wObj.write(method.getName()+QUOTE+COMMA+NEW_LINE);
+                wObj.write(CLASSNAME+QUOTE);
                 wObj.write(classFullName);
-                wObj.write("\" } \nexternal; \n");
+                wObj.write(QUOTE+NEW_LINE+RIGHTBRACE+EXTERNAL_KEYWORD+SEMICOLON);
+                wObj.write(NEW_LINE);
 
             }
             wObj.close();
--- lib/src/main/java/org/wso2/ei/b7a/TestJava.java
+++ lib/src/main/java/org/wso2/ei/b7a/TestJava.java
@@ -2,8 +2,17 @@ package org.wso2.ei.b7a;
 
 public class TestJava{
     public static void printText() {
+
         System.out.println("Hello World!");
     }
+
+    public static void getText(String name){
+        System.out.println(name);
+    }
+
+    /*public static String concatinateText(String a, String b){
+        return a+b;
+    }*/
 
     public static int add(int x,int y) {
         int sum=x+y;
